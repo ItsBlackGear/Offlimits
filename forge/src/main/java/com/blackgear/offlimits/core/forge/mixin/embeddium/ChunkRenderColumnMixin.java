f2746b0f22b73ceecddeb916348d76c2c45eb5ff
ItsBlackGear <itsblackgear@gmail.com>
2024-08-11 16:14:51 -0400

    🐛 fix #1

---
+++ forge/src/main/java/com/blackgear/offlimits/core/forge/mixin/embeddium/ChunkRenderColumnMixin.java
@@ -0,0 +1,20 @@
+package com.blackgear.offlimits.core.forge.mixin.embeddium;
+
+import com.blackgear.offlimits.Offlimits;
+import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderColumn;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Pseudo @Mixin(ChunkRenderColumn.class)
+public class ChunkRenderColumnMixin {
+    @ModifyConstant(
+        method = "<init>",
+        constant = @Constant(intValue = 16),
+        remap = false
+    )
+    private int offlimits$init(int original) {
+        return Offlimits.INSTANCE.getSectionsCount();
+    }
+}
