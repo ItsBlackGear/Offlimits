2b739cc2359dd08e5b3203de965b5cb517b1c6f7
ItsBlackGear <itsblackgear@gmail.com>
2024-09-01 23:30:19 -0400

    🐛 fix #6 #7

--- common/src/main/java/com/blackgear/offlimits/core/mixin/client/LevelRendererMixin.java
+++ common/src/main/java/com/blackgear/offlimits/core/mixin/client/LevelRendererMixin.java
@@ -2,9 +2,15 @@ package com.blackgear.offlimits.core.mixin.client;
 
 import com.blackgear.offlimits.Offlimits;
 import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
@@ -16,12 +22,24 @@ public class LevelRendererMixin {
         return Offlimits.INSTANCE.getMaxBuildHeight();
     }
     
+    @Inject(
+        method = "getRelativeFrom",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void off$getRelativeFrom(BlockPos playerPos, ChunkRenderDispatcher.RenderChunk renderChunkBase, Direction facing, CallbackInfoReturnable<ChunkRenderDispatcher.RenderChunk> cir) {
+        BlockPos pos = renderChunkBase.getRelativeOrigin(facing);
+        if (pos.getY() < Offlimits.INSTANCE.getMinBuildHeight() || pos.getY() >= Offlimits.INSTANCE.getMaxBuildHeight()) {
+            cir.setReturnValue(null);
+        }
+    }
+    
     @ModifyConstant(
         method = "renderWorldBounds",
         constant = @Constant(intValue = 256),
         expect = 8
     )
-    private int offlimits$renderWorldBounds(int original) {
+    private int off$renderWorldBounds(int original) {
         return Offlimits.INSTANCE.getMaxBuildHeight();
     }
 }
